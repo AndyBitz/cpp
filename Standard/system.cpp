c8ee504f8adb638b08aebafaede2b735dabbc21c
Andy Bitz <andy.schneider13@gmail.com>
2015-08-31 18:21:02 +0200

    Date: 31.08.15 Cpp Files Upload

---
+++ Standard/system.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	system("cls");
+	cout << "Hello World!" << endl;
+	cin.sync();
+	cin.get();
+	return 0;
+}
---
+++ cpp test/tstcpp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void read();
+
+int main(){
+
+	read();
+	
+	cin.sync();
+	cin.get();
+	
+}
+
+void read(){
+	
+	system("cd C:\Users");
+	system("dir > temp.txt");
+	system("attrib temp.txt +h");
+	
+	string lookfor;
+	
+	ifstream temp("temp.txt");
+	temp << 
+	
+	
+}
